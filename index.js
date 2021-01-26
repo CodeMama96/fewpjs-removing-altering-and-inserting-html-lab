43656de2665d0377ceb194279900deb7a573c4ac
Alicia Weenum <weenum.alicia@gmail.com>
2021-01-25 19:32:39 -0500

    commit tree 7c7a20da0bebf6ff3da2aec640a8c17df46d937c

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+main.remove()
+    
+// let element = document.createElement('h1')
+// document.body.appendChild(element) 
+
+// let newHeader = document.querySelector("h1#victory");
+// element.innerHTML = 'victory' 
 // Write your code here!
+
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory" //setting the id
+newHeader.innerHTML = "Alicia is the champion!"; //updates the string
+//newHeader.className = "victory"; (still passes?)
